d66106e67cfa409fb658bbdaf8d71ce4fa750771
pintubeast <abhishek.jk1992@gmail.com>
2023-08-13 11:08:03 +0530

    q2 done

--- aj4119_hw5_q2.cpp
+++ aj4119_hw5_q2.cpp
@@ -1,7 +1,45 @@
 #include<iostream>
 using namespace std;
+void printShiftedTriangle(int n, int m, char symbol);
+
+void printShiftedTriangle(int n, int m, char symbol)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m+n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=2*(i-1)+1;k++)
+        {
+            cout<<symbol;
+        }
+        cout<<"\n";
+    }
+
+}
+
+void printPineTree(int n, char symbol);
+
+void printPineTree(int n, char symbol)
+{
+    for(int i=1;i<=n;i++)
+    {
+        printShiftedTriangle(i+1,n-i,symbol);
+       
+    }
+
+}
+
+
 
 int main()
-{
+{  
+    int num;
+    char symbol;
+    cout<<"Please enter a positive integer and a character\n";
+    cin>>num>>symbol;
+    printPineTree(num,symbol);
+      
     return 0;
 }
