94a6bb705340bc3def746978f855bdbd86e48e34
pintubeast <abhishek.jk1992@gmail.com>
2023-08-13 11:55:56 +0530

    q3 completed

--- aj4119_hw5_q3.cpp
+++ aj4119_hw5_q3.cpp
@@ -30,16 +30,106 @@ int printMonthCalender(int numOfDays, int startingDay)
         dayCounter=8;
 
     }
-    
+    cout<<"\n\n";
     return dayCounter-1;
+
+}
+bool isLeapYear(int year);
+
+bool isLeapYear(int year)
+{   bool isLeap =false;
+    if(year % 400 ==0)
+    {
+        isLeap=true;
+    }
+    else if(year % 4==0 && year % 100 !=0)
+    {
+        isLeap=true;
+    }
+    return isLeap;
+
+    
+}
+
+void printYearCalender(int year, int startingDay);
+
+void printYearCalender(int year, int startingDay)
+{
+    
+    for(int i=1;i<=12;i++)
+    {
+        switch(i)
+        {
+            case 1:
+                cout<<"January "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break;
+            case 2:
+                cout<<"February "<<year<<"\n\n";
+                if(isLeapYear(year))
+                {
+                    startingDay=printMonthCalender(29,startingDay) % 7 + 1;
+                }
+                else
+                {
+                    startingDay=printMonthCalender(28,startingDay) % 7 + 1;
+
+                }
+                break;
+
+            case 3:
+                cout<<"March "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break;            
+            case 4:
+                cout<<"April "<<year<<"\n\n";
+                startingDay=printMonthCalender(30,startingDay) % 7 + 1; 
+                break;            
+            case 5:
+                cout<<"May "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break; 
+            case 6:
+                cout<<"June "<<year<<"\n\n";
+                startingDay=printMonthCalender(30,startingDay) % 7 + 1; 
+                break;                   
+            case 7:
+                cout<<"July "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break;  
+            case 8:
+                cout<<"August "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break; 
+            case 9:
+                cout<<"September "<<year<<"\n\n";
+                startingDay=printMonthCalender(30,startingDay) % 7 + 1; 
+                break;   
+            case 10:
+                cout<<"October "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break;    
+            case 11:
+                cout<<"November "<<year<<"\n\n";
+                startingDay=printMonthCalender(30,startingDay) % 7 + 1; 
+                break;  
+            case 12:
+                cout<<"December "<<year<<"\n\n";
+                startingDay=printMonthCalender(31,startingDay) % 7 + 1; 
+                break;                                                                                
+        }
+    }
 
 }
 int main()
 {   
-    int numDays,startingDay;
-    cout<<"Please enter the number of Days and the starting day(Monday=1 to Sunday=7): ";
-    cin>>numDays>>startingDay;
-    int x= printMonthCalender(numDays,startingDay);
-    cout<<"\n The last day of the Month is: "<<x;
+    int year,startingDay;
+    
+    cout<<"Please enter a year and the starting day(Monday=1 to Sunday=7): ";
+    cin>>year>>startingDay;
+    //int x= printMonthCalender(numDays,startingDay);
+   // cout<<"\n The last day of the Month is: "<<x;
+    
+    printYearCalender(year,startingDay);
 
 }
