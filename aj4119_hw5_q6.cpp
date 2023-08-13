9f9ea9834aec560d2e7a45900d01c2b086e6f0a2
pintubeast <abhishek.jk1992@gmail.com>
2023-08-13 14:14:40 +0530

    q6 done:

---
+++ aj4119_hw5_q6.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+double factorial=1.0;
+void calcFactorial(double& factorial, int num)
+{    
+    factorial *= num; 
+}
+
+double eApprox(int n)
+{ 
+    double e=1.0;
+    factorial=1.0;
+    for(int i=1;i<=n;i++)
+    {   
+        calcFactorial(factorial,i);
+        e += 1.0/factorial; 
+        
+    }
+    return e;
+}
+int main()
+{
+    
+    int num;
+    cout<<"Please enter a positive integer n: ";
+    cin>>num;
+
+    cout.precision(30);
+
+    for(int n=1;n<=num;n++)
+    {   
+        cout<<"n = "<<n<<"\t"<<eApprox(n)<<"\n";
+    }
+    
+    
+    return 0;
+}
